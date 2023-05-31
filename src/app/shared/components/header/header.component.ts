af5f812056bbb4082638118d259b79f197b383ed
Jesus Hernandez <jehersi988@gmail.com>
2023-05-30 21:50:13 -0500

    header: validando subjects(observables)

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  quantity$ = this._shoppingCartService.quantityAction$
+  total$ = this._shoppingCartService.totalAction$
+  cart$ = this._shoppingCartService.cartAction$
+
+  constructor(public _shoppingCartService: ShoppingCartService) { }
+
+  ngOnInit(): void {
+    
+  }
 }
